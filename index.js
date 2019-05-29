1a96d2998290dda262dd4afee71f2b61e963d35b
Bernardo Vieira <bernardo5@live.com.pt>
2019-05-29 14:59:40 +0100

    fix input values and return promise

--- index.js
+++ index.js
@@ -9,39 +9,52 @@ const templateFile = 'template/index.html';
 /**
  * Transform an input into a pdf considering the input in the body
  * of an HTML page.
- * @param filePathInput path for input file
+ * @param outputDir output folder path
+ * @param outputFile output filename
+ * @param inputContent content to be rendered
  */
-exports.generatePDF = (outputDir, outputFileName, inputContent) => {
-    (async () => {
-        // start a browser with puppeter
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.setJavaScriptEnabled(true);
-        // we assume, the input received externally, only have the body
-        // so here, we have a template in order to apply styles
-        const templatePath = path.join(__dirname, templateFile);
-        const templateContent = String(fs.readFileSync(templatePath));
-        // put all data together
-        const view = {
-            content: inputContent,
-        };
-        // calls the render engine
-        const output = Mustache.render(templateContent, view);
-        // verify if the docs/ folder exist and creates it if not
-        const destinationDocsFolderPath = `${process.cwd()}/${outputDir}/`;
-        if (!fs.existsSync(destinationDocsFolderPath)) {
-            fs.mkdirSync(destinationDocsFolderPath);
+exports.generatePDF = (outputDir, outputFile, inputContent) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            // start a browser with puppeter
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.setJavaScriptEnabled(true);
+            // we assume, the input received externally, only have the body
+            // so here, we have a template in order to apply styles
+            const templatePath = path.join(__dirname, templateFile);
+            const templateContent = String(fs.readFileSync(templatePath));
+            // put all data together
+            const view = {
+                content: inputContent,
+            };
+            // calls the render engine
+            const output = Mustache.render(templateContent, view);
+            // verify if the docs/ folder exist and creates it if not
+            const destinationDocsFolderPath = path.join(process.cwd(), outputDir);
+            if (!fs.existsSync(destinationDocsFolderPath)) {
+                fs.mkdirSync(destinationDocsFolderPath);
+            }
+            // write it to a file
+            let outputFileName;
+            if (path.extname(outputFile).length > 0) {
+                outputFileName = path.parse(outputFile).name;
+            } else {
+                outputFileName = outputFile;
+            }
+            const outputHTMLFilePath = path.join(process.cwd(), outputDir, `${outputFileName}.html`);
+            const outputPDFFilePath = path.join(process.cwd(), outputDir, `${outputFileName}.pdf`);
+            fs.writeFileSync(outputHTMLFilePath, output);
+            // now, go to page
+            await page.goto(`file://${outputHTMLFilePath}`, { waitUntil: 'networkidle2' });
+            await page.emulateMedia('screen');
+            // generate the pdf
+            await page.pdf({ path: outputPDFFilePath, format: 'A4' });
+            // close the browser
+            await browser.close();
+        } catch(e) {
+            reject(e);
         }
-        // write it to a file
-        const outputHTMLFilePath = path.join(process.cwd(), outputDir, `${outputFileName}.html`);
-        const outputPDFFilePath = path.join(process.cwd(), outputDir, `${outputFileName}.pdf`);
-        fs.writeFileSync(outputHTMLFilePath, output);
-        // now, go to page
-        await page.goto(`file://${outputHTMLFilePath}`, { waitUntil: 'networkidle2' });
-        await page.emulateMedia('screen');
-        // generate the pdf
-        await page.pdf({ path: outputPDFFilePath, format: 'A4' });
-        // close the browser
-        await browser.close();
-    })();
+        resolve(0);
+    });
 }
